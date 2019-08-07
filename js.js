6f21a6d0f8cf376a8fb130eff369e1dbca2e22d5
md-one <mehdi.black555@gmail.com>
2019-08-07 13:10:51 +0100

    add Products page
    add filer to mune,
    + style the mune = anim

--- js.js
+++ js.js
@@ -1,27 +1,66 @@
-var btn = document.getElementById("Hamburger");
-var svg = document.querySelector("#Hamburger svg")
+var intro = document.getElementById("intro");
+var Products = document.getElementById("productPanal");
+var homeBtn = document.getElementById("homeBtn");
 
+var li = document.querySelectorAll("#menu li");
+var allProdact = document.querySelectorAll(".product");
+var newProdact = document.querySelectorAll(".new");
+var officeProdact = document.querySelectorAll(".office");
+
+
+function goToProducts() {
+    intro.style.display = "none";
+    Products.style.display = "block";
+}
+
+function goToIntro() {
+    Products.style.display = "none";
+    intro.style.display = "block";
+}
+
+homeBtn.addEventListener("click", function () {
+    goToIntro()
+});
+
+for (let i = 0; i < li.length; i++) {
+    li[i].addEventListener("click", function () {
+        menu.style.transform = "translateX(100%)"
+        goToProducts();
+    })
+}
+
+function hideAllProducts() {
+    for (let i = 0; i < allProdact.length; i++) {
+        allProdact[i].style.display = "none";
+    }
+}
+
+li[1].addEventListener("click", function () {
+
+    hideAllProducts();
+    for (let i = 0; i < newProdact.length; i++) {
+        newProdact[i].style.display = "flex";
+    }
+});
+li[4].addEventListener("click", function () {
+
+    for (let i = 0; i < allProdact.length; i++) {
+        allProdact[i].style.display = "flex";
+        console.log("gggggggggg");
+
+    }
+});
+
+var menuBtn = document.getElementById("Hamburger");
+var menu = document.getElementById("menu");
 var menuOpen = false;
 
-
-
-
-btn.addEventListener("click", function () {
-
-    var menu = document.getElementById("menu");
-
-
-
+menuBtn.addEventListener("click", function () {
     if (menuOpen === false) {
         menuOpen = true;
-
         menu.style.transform = "translateX(0)"
-
-
     } else {
         menuOpen = false;
-
         menu.style.transform = "translateX(100%)"
-
     }
 });
