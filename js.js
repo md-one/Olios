4f8cdc2f6188deb1ab9616597a557219cbddbce3
md-one <mehdi.black555@gmail.com>
2019-08-14 23:34:31 +0100

    adding single product page
    by copying value from click on product
    at products page and past them in product page

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 var intro = document.getElementById("intro");
-var Products = document.getElementById("productPanal");
+var Products = document.getElementById("products");
+var Product = document.getElementById("product");
 var search = document.getElementById("searchPanal");
 var homeBtn = document.getElementById("homeBtn");
 var li = document.querySelectorAll("#menu li");
@@ -9,10 +10,15 @@ var livingRoomProdact = document.querySelectorAll(".livingRoom");
 var officeProdact = document.querySelectorAll(".office");
 var bedRoomProdact = document.querySelectorAll(".bedRoom");
 
-
 function goToProducts() {
     intro.style.display = "none";
     Products.style.display = "block";
+}
+
+function goToProduct() {
+    intro.style.display = "none";
+    Products.style.display = "none";
+    Product.style.display = "block";
 }
 
 function goToIntro() {
@@ -65,8 +71,6 @@ li[3].addEventListener("click", function () {
 
     for (let i = 0; i < allProdact.length; i++) {
         allProdact[i].style.display = "flex";
-        console.log("gggggggggg");
-
     }
 });
 
@@ -83,3 +87,25 @@ menuBtn.addEventListener("click", function () {
         menu.style.transform = "translateX(100%)"
     }
 });
+
+
+function showproduct(obj) {
+    goToProduct();
+    var img = obj.querySelector("img").src;
+    var prodectimg = document.querySelector("#product .img img");
+    prodectimg.src = img;
+
+    var h4 = obj.querySelector(".text .name").outerHTML;
+    var name = document.querySelector("#product .text .name");
+    name.innerHTML = h4;
+
+    var p = obj.querySelector(".text .description").outerHTML;
+    var description = document.querySelector("#product .text .description");
+    description.innerHTML = p;
+
+
+    var h5 = obj.querySelector(".text .cost").outerHTML;
+    var Cost = document.querySelector("#product .text .cost");
+    Cost.innerHTML = h5;
+
+}
